94f1cc25d135f013cf1ae9c688a4e97fe321a3d5
Olabi Elore <olabielore@gmail.com>
2025-10-20 15:27:51 +0300

    task-2

--- js/app.js
+++ js/app.js
@@ -9,5 +9,22 @@ const str = "abcde";
 function checkSymbol(str) {
   return str.startsWith("a") ? "так" : "ні";
 }
-console.log(checkSymbol(str));
+// console.log(checkSymbol(str));
 // console.log(checkSymbol("bcde"));
+
+//TODO:============task-2=========================
+// Якщо ваше ім’я містить більше 4 букв і прізвище більше 5, то виведіть на екран число, яке дорівнює сумі букв імені і прізвища.
+// Якщо умова не виконується - вивести “неможливо виконати умову”.
+// Наприклад: “Віктор Сташко” Віктор - 6 літер, Сташко - 6 літер. Виводимо 12.
+// Використовувати вбудований метод length.
+
+const firstName = "Elore";
+const lastName = "Olabi";
+function sumLetters(firstName, lastName) {
+  const first = firstName.length;
+  const last = lastName.length;
+  return first > 4 && last > 5 ? first + last : "неможливо виконати умову";
+}
+
+// console.log(sumLetters(firstName, lastName));
+// console.log(sumLetters("Віктор", "Сташко"));
