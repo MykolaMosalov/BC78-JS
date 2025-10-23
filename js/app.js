20dbfb4fe402bbf4ac5ce725a33354159a16184e
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2025-10-23 15:11:48 +0300

    intro

---
+++ js/app.js
@@ -0,0 +1,79 @@
+// const str =
+//   'Dog quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy hot-dog ?';
+
+// let strArray = str.split(' ');
+
+// const newArray = [];
+
+// const someArr = [10, 29, 69, 49, 59, true, false, undefined];
+
+// const numbersArray = [];
+
+// for (const item of someArr) {
+//   if (typeof item === 'number' && item % 2 !== 0) {
+//     numbersArray.push(item);
+//   }
+// }
+
+// console.log(numbersArray);
+
+// for (const value of strArray) {
+//   // if (value === 'dog') {
+//   //   newArray.push('monkey');
+//   //   continue;
+//   // }
+
+//   // if (value === 'dog.') {
+//   //   newArray.push('monkey.');
+//   //   continue;
+//   // }
+
+//   // if (value === 'Dog') {
+//   //   newArray.push('Monkey');
+//   //   continue;
+//   // }
+
+//   // newArray.push(value);
+
+//   switch (value) {
+//     case 'dog':
+//       newArray.push('monkey');
+//       break;
+
+//     case 'Dog':
+//       newArray.push('Monkey');
+//       break;
+
+//     case 'dog.':
+//       newArray.push('monkey.');
+//       break;
+
+//     default:
+//       newArray.push(value);
+//   }
+// }
+
+// console.log(newArray.join(' '));
+
+// const arr = [10, 20, 40];
+
+// arr.push(29);
+// arr.pop();
+// arr.unshift(0);
+// arr.shift();
+
+// const newArr = [0].concat(arr);
+// console.log(newArr);
+
+// arr.unshift(87);
+
+// arr[1] = 100;
+
+// const idx = arr.indexOf(10);
+// console.log(idx);
+
+// if (idx !== -1) {
+//   arr[idx] = 100;
+// }
+
+// console.log(arr);
