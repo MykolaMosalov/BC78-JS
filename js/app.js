71ce77f3633e8503fe433aec64c2b26431aeaed0
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2025-10-27 14:34:45 +0200

    intro

---
+++ js/app.js
@@ -0,0 +1,63 @@
+// const users = [
+//   {
+//     userName: 'Bob',
+//     age: 34,
+//     isActive: true,
+//     address: {
+//       city: 'Lviv',
+//       str: 'Kovpaka',
+//     },
+//   },
+//   'Mukola',
+//   'Sergii',
+// ];
+
+// const user = {
+//   userName: 'Bob',
+//   age: 34,
+//   isActive: true,
+//   address: {
+//     city: 'Lviv',
+//     str: 'Kovpaka',
+//   },
+// };
+
+// const newUser = user;
+// const newUser = Object.create(user);
+// const newUser = { ...user };
+// newUser.userName = 'Mango';
+// newUser.address.city = 'Kiyv';
+// console.log(newUser);
+// console.log(user);
+
+// for (const key in user) {
+//   console.log(user[key]);
+// }
+
+// const keys = Object.keys(user);
+// console.log(keys);
+
+// const value = Object.values(user);
+// console.log(value);
+
+// const res = Object.entries(user);
+// console.log(res);
+
+// const obj = {
+//   Mango: 2500,
+//   Bob: 2200,
+// };
+
+// let total = 0;
+
+// for (const key in obj) {
+//   total += obj[key];
+// }
+
+// console.log(total);
+
+// for (const item of Object.values(obj)) {
+//   total += item;
+// }
+
+// console.log(total);
