8557f376d0bea0ee55a1de86125238ac2d2beab1
Artem Gardysh <artemgardysh@gmail.com>
2025-10-29 14:10:16 +0000

    task-7

--- js/app.js
+++ js/app.js
@@ -154,3 +154,34 @@
 //   }
 // }
 // console.log(serchNumber(min, max, 45));
+
+//TODO:============task-08=========================
+// Перевірити два масиву і дізнатися, чи вони рівні
+
+//* Оцікуваний результат - true, true, false, false
+const arr1 = [1, 4, 6, "color", 324, 232, "list", 11, 9, "dream", 34, 0, -30];
+const arr2 = [6, "dream", -30, 11, 9, 1, 324, 34, "color", 4, 232, 0, "list"];
+const arr3 = [4, 232, 6, -30, "color", 324, "list", 1, 11, 9, "dream", 34, 0];
+const arr4 = ["color", 6, -30, 11, 9, 1, "dream", 324, 34, 4, 232, 0, "list"];
+const arr5 = [1, 4, 6, "color", "list", 11, 9, "dream", 34, 0, -30, "lesson"];
+const arr6 = [6, 324, "dream", -30, 9, 8, 34, "color", 4, 232, 0, "list", 11];
+const arr7 = [1, 4, 6, "color", 324, 232, "list", 11, 9, "dream", 34, 0, -30];
+const arr8 = [6, "dream", -30, 10, 9, 1, 324, 34, "color", 4, 232, 0, "list"];
+
+function compareArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (const char of arr1) {
+    if (!arr2.includes(char)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(compareArrays(arr1, arr2)); // true
+console.log(compareArrays(arr3, arr4)); // true
+console.log(compareArrays(arr5, arr6)); // false
+console.log(compareArrays(arr7, arr8)); // false
