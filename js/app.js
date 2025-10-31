9f47f8ef152c54304b1752fd5f4105b1b15a1c2c
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2025-10-31 15:09:50 +0200

    intro

---
+++ js/app.js
@@ -0,0 +1,74 @@
+// const students = [
+//   { name: 'Андрій', surname: 'Іванов', grade: 4.5 },
+//   { name: 'Олександр', surname: 'Петров', grade: 3.9 },
+//   { name: 'Марія', surname: 'Сидорова', grade: 4.8 },
+//   { name: 'Ірина', surname: 'Федорова', grade: 4.2 },
+// ];
+// const number = [1, 4, 6, 70];
+
+//! Map
+// const namesArr = students.map((item) => item.grade + 1); // []
+// console.log(namesArr);
+// console.log(students);
+
+//! Filter
+// const topStudents = students.filter(({ grade }) => grade < 4); // []
+// console.log(topStudents);
+
+//! Find
+// const findSidorova = students.find(({ surname }) => surname === 'Сидорова'); // el
+// console.log(findSidorova);
+
+// const findFirstEvenElement = number.find((number) => number > 3); // el
+// console.log(findFirstEvenElement);
+
+//! Sort
+// const sortGrades = [...students].sort((a, b) => a.grade - b.grade);
+// console.log(sortGrades);
+
+// console.log(students);
+
+//! toSorted
+// const sortGrades = students.toSorted((a, b) => a.grade - b.grade); // []
+// console.log(sortGrades);
+
+// const sortedNamesArr = students.toSorted((a, b) =>
+//   a.name.localeCompare(b.name)
+// );
+// console.log(sortedNamesArr);
+
+// const numbers = [2, 200, 3, 1];
+// console.log(numbers.toSorted((a, b) => a - b));
+
+//! some every - Boolean
+
+//! FlatMap
+// const arr1 = [1, 2, 3, 4];
+// const arr2 = [5, 6, 7, 8];
+
+// const data = [
+//   { name: 'Андрій', surname: 'Іванов', grade: [1, 2, 3, 4] },
+//   { name: 'Олександр', surname: 'Петров', grade: [5, 6, 7, 8] },
+// ];
+
+// console.log(arr1.concat(arr2));
+// console.log([...arr1, ...arr2]);
+
+// console.log(data.flatMap(({ grade }) => grade)); // []
+
+//! reduce
+// const number = [1, 4, 6, 70];
+
+// console.log(number.reduce((acc, number) => acc + number, 0)); // el
+
+// const res = students
+//   .filter(({ grade }) => grade > 4)
+//   .toSorted((a, b) => a.name.localeCompare(b.name))
+//   .map(({ name }) => name)
+//   .some((word) => word === 'Ірина');
+
+// console.log(res);
+
+//! new Set
+// const number = [1, 4, 6, 70, 1, 6];
+// console.log([...new Set(number)]);
