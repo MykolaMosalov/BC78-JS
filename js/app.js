8bd6a053803ab818988a69d330472e2ec1abfc3b
Mr_Whoopee <artem.holovko.97@gmail.com>
2025-11-05 18:32:39 +0300

    task-08, task-01

--- js/app.js
+++ js/app.js
@@ -90,8 +90,8 @@
 // У кожному масиві якщо унікальний рядок, у якому не повторюються літери.
 // Знайдіть і виведіть цей рядок.
 
-const stringArr = ["abc", "acb", "bac", "foo", "bca", "cab", "jhgf", "cba"];
-const stringArr2 = ["fghj", "ghfj", "abcd", "jhgf", "fghj", "fgjh", "ghjf"];
+// const stringArr = ["abc", "acb", "bac", "foo", "bca", "cab", "jhgf", "cba"];
+// const stringArr2 = ["fghj", "ghfj", "abcd", "jhgf", "fghj", "fgjh", "ghjf"];
 
 // ["bac", "acb", "bac", "foo", "bca", "cab", "cba"] // === 'foo'
 // ['fghj', 'ghfj', 'abcd', 'jhgf', 'fghj', 'fgjh', 'ghjf'] // === 'abcd'
@@ -133,3 +133,142 @@ const stringArr2 = ["fghj", "ghfj", "abcd", "jhgf", "fghj", "fgjh", "ghjf"];
 
 // console.log(searchUniq(stringArr));
 // console.log(searchUniq(stringArr2));
+
+//! this and contacts
+//TODO:=================task-08=============================
+// Виклик checkPassword() у наведеному нижче коді повинен перевірити пароль
+// та викликати user.loginOk / loginFail залежно від відповіді.
+
+// //? Однак його виклик призводить до помилки. Чому?
+
+// const checkPassword = function (ok, fail, password) {
+//   password === "rockstar" ? ok() : fail();
+// };
+
+// const client = {
+//   name: "Вася",
+
+//   loginOk() {
+//     console.log(`${this.name} logged in`);
+//   },
+
+//   loginFail() {
+//     console.log(`${this.name} failed to log in`);
+//   },
+// };
+
+// checkPassword(
+//   client.loginOk.bind(client),
+//   client.loginFail.bind(client),
+//   "rockstar"
+// );
+
+//! class
+//TODO:=================task-01=============================
+// Завдання полягає у створенні програми, що дозволяє керувати замовленнями в ресторані.
+
+// #### Для цього потрібно реалізувати клас `Order`, який містить такі приватні властивості:
+// - `tableNumber` - номер столика, де зроблено замовлення
+// - `items` - масив предметів замовлення, де кожен предмет містить ім'я (name) та ціну (price)
+// - `isPaid` - позначає, чи оплачене замовлення (default = false)
+
+// ##### Для класу Order потрібно реалізувати такі методи:
+// - `calculateTotal`() - повертає загальну суму замовлення.
+// - `markAsPaid`() - позначає замовлення як оплачене
+// - `addItem`() - який додає предмет замовлення до масиву "items"
+// - `removeItem`() - який видаляє предмет замовленя з масиву "items"
+// ##### Додатково можна створити гетери та сетери для отримання номера столика, статусу замовлення та самого замовлення
+
+// #### Також потрібно створити клас `MenuItem`, який містить властивості `name` та `price` для предметів замовлення.
+
+class Order {
+  #tableNumber;
+  #items;
+  #isPaid;
+
+  constructor(table) {
+    this.#tableNumber = table;
+    this.#items = [];
+    this.#isPaid = false;
+  }
+
+  get tableNumber() {
+    return this.#tableNumber;
+  }
+
+  set tableNumber(newTable) {
+    this.#tableNumber = newTable;
+  }
+
+  get isPaid() {
+    return this.#isPaid;
+  }
+
+  set isPaid(newPaid) {
+    this.#isPaid = newPaid;
+  }
+
+  calculateTotal() {
+    return this.#items.reduce((acc, { price }) => (acc += price), 0);
+  }
+
+  markAsPaid() {
+    this.#isPaid = true;
+    console.log(this.#isPaid);
+  }
+
+  addItem(item) {
+    this.#items.push(item);
+  }
+
+  removeItem(item) {
+    for (const i = 0; i < this.#items.length; i++) {
+      if (item.name === this.#items[i].name) {
+        this.#items.splice(i, 1);
+        break;
+      }
+    }
+  }
+}
+
+class MenuItem {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+}
+
+// Create menu items
+const burger = new MenuItem("burger", 100);
+const pizza = new MenuItem("pizza", 500);
+const coffee = new MenuItem("coffee", 80);
+
+// New Order
+const order = new Order(1);
+const order1 = new Order(2);
+const order2 = new Order(3);
+
+// Add items to order
+order.addItem(burger);
+order.addItem(coffee);
+
+order1.addItem(pizza);
+
+order2.addItem(coffee);
+order2.addItem(pizza);
+
+// Calculate total price
+const total = order.calculateTotal();
+order.markAsPaid();
+console.log(`Order for table ${order.tableNumber} - Total: ${total} UAH`);
+console.log(`Is Paid: ${order.isPaid}`);
+
+const total1 = order1.calculateTotal();
+order1.markAsPaid();
+console.log(`Order for table ${order1.tableNumber} - Total: ${total1} UAH`);
+console.log(`Is Paid: ${order1.isPaid}`);
+
+const total2 = order2.calculateTotal();
+order2.markAsPaid();
+console.log(`Order for table ${order2.tableNumber} - Total: ${total2} UAH`);
+console.log(`Is Paid: ${order2.isPaid}`);
