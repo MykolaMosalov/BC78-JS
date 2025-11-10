f43a9f5ee6b74126763b4a3ea63660770c50b521
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2025-11-10 14:51:30 +0200

    intro

---
+++ js/app.js
@@ -0,0 +1,42 @@
+// const lisEl = document.querySelectorAll()
+// const lisEl = document.querySelectorAll('.calendar-item');
+
+// lisEl.forEach((li) =>
+//   li.addEventListener('click', () => {
+//     console.log('click');
+//   })
+// );
+
+// const CalendarListContainer = document.querySelector('.calendar-list');
+
+// CalendarListContainer.addEventListener('click', onCalendarListClick);
+
+// function onCalendarListClick(e) {
+//   const target = e.target;
+//   const activeTarget = CalendarListContainer.querySelector('.active');
+
+//   if (target.nodeName !== 'LI') return;
+
+//   activeTarget.classList.remove('active');
+//   target.classList.add('active');
+
+//   // console.log(e);
+//   // console.log('currentTarget:', e.currentTarget);
+//   // console.log('Target:', e.target);
+// }
+
+// Модуль 8. Заняття 13. Поширення подій.
+
+//TODO:=========task-01=======Accordion==========
+/**
+ * Викоритовуй шаблон акордеон меню з файлу html та напиши наступний функціонал:
+ * при кліку на елемент меню, розкривай блок з текстом.При повторному кліку по елементу,
+ * розкритий текст приховується.Нажимаючи на інші елементи меню, попередньо відкриті елементи не закриваються.
+ */
+
+//TODO:=========task-02=======Pagination==========
+/**
+ * Викоритовуй шаблон пагінації з файлу html для розробки функціоналу пагінатора.
+ * Використовуй поширення подій.Додавай клас`active` на поточну сторінку. Створи допоміжну функцію`updateActivePage`, яка повинна обновляти активний клас.
+ * Функція`handleClick`повинна викликатися коли відбувається клік на елементі`pagination`.
+ */
