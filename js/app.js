f8f466d9644363a4ba85c3d6ac7a10629ffeb81a
Olesia Tsaturian <olesiases@gmail.com>
2025-11-14 16:24:26 +0200

    task 1

--- js/app.js
+++ js/app.js
@@ -1,16 +1,35 @@
 import {
-  addDataToLocalStorage,
-  getDataFromLocalStorage,
-  removeDataFromLocalStorage,
-  removeAllDataFromLocalStorage,
+    addDataToLocalStorage,
+    getDataFromLocalStorage,
+    removeDataFromLocalStorage,
+    removeAllDataFromLocalStorage,
 } from "./localStorage-api.js";
+import { LS_KEY } from "./keys.js";
 
-addDataToLocalStorage("test", [10, 20, 30]);
-addDataToLocalStorage("test2", 10);
+import refs from "./refs.js";
 
-console.log(getDataFromLocalStorage("test"));
-console.log(getDataFromLocalStorage("test2"));
+// - Використовуй html з файлу [themeSwitch.html](./themeSwitch.html)
+// - Використовуй local storage для зберігання вибраної теми (dark / light)
+//
 
-removeDataFromLocalStorage("test2");
+const onCheckboxClick = (event) => {
+    if (event.target.checked) {
+        refs.body.classList.replace("light", "dark");
+        addDataToLocalStorage(LS_KEY, "dark");
+    } else {
+        refs.body.classList.replace("dark", "light");
+        addDataToLocalStorage(LS_KEY, "light");
+    }
+};
 
-removeAllDataFromLocalStorage();
+const renderPage = () => {
+    if (getDataFromLocalStorage(LS_KEY) === "dark") {
+        refs.body.classList.replace("light", "dark");
+        refs.checkbox.checked = true;
+    } else {
+        refs.body.classList.replace("dark", "light");
+    }
+};
+
+refs.checkbox.addEventListener("click", onCheckboxClick);
+document.addEventListener("DOMContentLoaded", renderPage);
---
+++ js/refs.js
@@ -0,0 +1,6 @@
+const refs = {
+    body: document.body,
+    checkbox: document.querySelector("#theme-switch"),
+};
+
+export default refs;
